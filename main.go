e08c4f280ff6670119568c559d5591a6789f1a8d
Matthias Loibl <mail@matthiasloibl.com>
2016-07-03 18:21:45 +0200

    Use server sent events to push logs to the UI

--- main.go
+++ main.go
@@ -8,62 +8,39 @@ package main
 
 import (
 	"bufio"
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"sync"
 	"time"
 )
 
-// brainstorm struct
-// type logInstance struct {
-// 	name       string
-// 	port       int
-// 	instanceID string //some sort of unique ID for inter-OS stuff
-// 	createdTime
-// 	ownerProcessID
-// 	ownerShell
-// }
-
 type logEntry struct {
-	line      string
-	timestamp time.Time
+	Line      string    `json:"line"`
+	Timestamp time.Time `json:"timestamp"`
 }
 
 const port = 18192
 
-func logEntryToOutput(le logEntry) string {
-	return fmt.Sprintf("timestamp=%s, content=%s\n", le.timestamp, le.line)
-}
-
 func main() {
-	fmt.Println("hello world")
-
-	var logs []logEntry
-	var logsMutex sync.Mutex
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "")
-		//dont wait for channel
-		logsMutex.Lock()
-		for _, le := range logs {
-			fmt.Fprint(w, logEntryToOutput(le))
-		}
-		logsMutex.Unlock()
-	})
+	broker := NewBroker()
 
 	go func() {
 		scanner := bufio.NewScanner(os.Stdin)
 		for scanner.Scan() {
 			test := scanner.Text()
-			fmt.Println(test)
-			logsMutex.Lock()
-			logs = append(logs, logEntry{line: test, timestamp: time.Now()})
-			logsMutex.Unlock()
+			le := logEntry{Line: test, Timestamp: time.Now()}
+
+			output, err := json.Marshal(le)
+			if err != nil {
+				continue // TODO: What's better?
+			}
+
+			broker.Notifier <- []byte(output)
 		}
 	}()
 
-	Open("http://localhost:18192")
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), nil))
+	//Open(fmt.Sprintf("http://localhost:%d", port))
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", port), broker))
 }
