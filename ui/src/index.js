2c303b7a6420e7b8d1bbad563217b7481811cc76
Matthias Loibl <mail@matthiasloibl.com>
2016-07-03 19:28:44 +0200

    Bind to the SSE with React components

--- ui/src/index.js
+++ ui/src/index.js
@@ -3,6 +3,29 @@ import ReactDOM from 'react-dom';
 
 export default
 class App extends React.Component {
+
+    componentDidMount() {
+        if (!!window.EventSource) {
+            var source = new EventSource('/logs');
+
+            source.addEventListener('message', function (e) {
+                console.log(e.data);
+            }, false);
+
+            source.addEventListener('open', function (e) {
+                console.log('listening for new logs');
+            }, false);
+
+            source.addEventListener('error', function (e) {
+                if (e.readyState == EventSource.CLOSED) {
+                    alert("Connection closed");
+                }
+            }, false);
+        } else {
+            alert("Upgrade your browser, WTF");
+        }
+    }
+
     render() {
         return (
             <div>Hello Logs</div>
